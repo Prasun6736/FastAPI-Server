3c523fc59d8d89c05333879c799f613ba81b1539
Prasun6736 <prasunmridha6@gmail.com>
2025-03-15 18:12:22 +0530

    in memory delete and update

--- app.py
+++ app.py
@@ -74,6 +74,13 @@ class Book(BaseModel):
     page_count: int
     language: str
 
+class UpdateBook(BaseModel):
+    id: int
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
 
 @app.get('/books',response_model=List[Book])
 async def get_books():
@@ -86,12 +93,29 @@ async def create_book(book_data:Book)->dict:
     return new_book
 
 @app.get('/book/{books_id}')
-async def update_book(books_id:int)->dict:
+async def get_book(books_id:int)->dict:
     for book in books:
         if book['id']==books_id:
             return book
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='Book not found')
 
-@app.get('/books/{book_id}')
-async def delete_book()-> dict:
-    pass
+@app.patch('/books/{book_id}')
+async def update(book_id:int,book:UpdateBook)-> dict:
+    for b in books:
+        if b['id']==book_id:
+            b['title']=book.title
+            b['author']=book.author
+            b['publisher']=book.publisher
+            b['page_count']=book.page_count
+            b['language']=book.language
+            return b
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='Book not found')
+
+@app.delete('/books/{book_id}',status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id:int):
+    for i in books:
+        if i['id']==book_id:
+            books.remove(i)
+
+            return {}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='Book not found')
